77a4a21848c7e3bb928d23f04d47564fdf9ab75b
j-goodman <johncurtisgoodman@gmail.com>
2026-06-18 20:44:52 -0400

    Implement chatbot request with example prompt.

--- script.js
+++ script.js
@@ -2,5 +2,20 @@ const form = document.getElementById("chat-input-form")
 const responseBox = document.getElementById("chatbot-response")
 
 form.onsubmit = async (event) => {
-    event.preventDefault
+    event.preventDefault()
+
+    const prompt = "What is the capital of Senegal?"
+
+    const response = await fetch(`http://localhost:11434/api/generate`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+            model: 'llama3.2',
+            prompt: prompt,
+            stream: false
+        })
+    })
+
+    const data = await response.json()
+    responseBox.innerText = data.response
 }
