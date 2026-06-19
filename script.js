fc5d107bc66a63f4cff921a043da2eee35aca54b
j-goodman <johncurtisgoodman@gmail.com>
2026-06-18 20:59:55 -0400

    Use actual user inputs as prompts.

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
 const form = document.getElementById("chat-input-form")
 const responseBox = document.getElementById("chatbot-response")
+const promptBox = document.getElementById("prompt")
 
 form.onsubmit = async (event) => {
     event.preventDefault()
 
-    const prompt = "What is the capital of Senegal?"
+    const prompt = promptBox.value
+    promptBox.value = ""
+    responseBox.innerText = "..."
 
     const response = await fetch(`http://localhost:11434/api/generate`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
             model: 'llama3.2',
-            prompt: prompt,
+            prompt: `${prompt}. Please keep responses between one and three sentences.`,
             stream: false
         })
     })
